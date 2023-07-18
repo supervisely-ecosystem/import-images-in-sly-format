d2692a0e3c57a7028b2ed74a89d7601b3662fa85
cxnt <pavelbartsits@gmail.com>
2023-07-18 19:08:33 +0400

    add download from ds ninja link

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -1,24 +1,14 @@
 import os
-import sys
-from pathlib import Path
-
-import supervisely as sly
+from supervisely.api.api import Api
 from supervisely.app.v1.app_service import AppService
 from supervisely.io.fs import mkdir
 
-root_source_dir = str(Path(sys.argv[0]).parents[1])
-print(f"App source directory: {root_source_dir}")
-sys.path.append(root_source_dir)
-
 # only for convenient debug
-# from dotenv import load_dotenv
-# debug_env_path = os.path.join(root_source_dir, "debug.env")
-# secret_debug_env_path = os.path.join(root_source_dir, "secret_debug.env")
-# load_dotenv(debug_env_path)
-# load_dotenv(secret_debug_env_path, override=True)
+from dotenv import load_dotenv
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-
-api: sly.Api = sly.Api.from_env()
+api: Api = Api.from_env()
 my_app: AppService = AppService()
 
 TEAM_ID = int(os.environ["context.teamId"])
@@ -27,6 +17,10 @@ TASK_ID = int(os.environ["TASK_ID"])
 
 INPUT_DIR: str = os.environ.get("modal.state.slyFolder", None)
 INPUT_FILE: str = os.environ.get("modal.state.slyFile", None)
+
+DS_NINJA_LINK: bool = False
+if INPUT_FILE.startswith("https://") or INPUT_FILE.startswith("http://"):
+    DS_NINJA_LINK = True
 
 if INPUT_DIR:
     IS_ON_AGENT = api.file.is_on_agent(INPUT_DIR)
